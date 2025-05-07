ce3108ce0e34c7fd408c82cfaaaf880efb9ec414
0xhaxk <0xhaxk@noreply.codeberg.org>
2025-05-07 13:59:24 +0000

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -6,15 +6,18 @@ set -e
 echo "[+] Launching nmtui to configure network..."
 nmtui
 
+echo "[+] Enable Arch Linux repositories..."
+sudo pacman -S --noconfirm artix-archlinux-support
+
+echo "[+] Inserting [extra] repo after [galaxy] in pacman.conf..."
+sudo sed -i '/^\[galaxy\]/a \
+[extra]\nInclude = /etc/pacman.d/mirrorlist-arch\n' /etc/pacman.conf
+
 echo "[+] Updating system and installing essential packages..."
-sudo pacman -Syu --noconfirm base-devel artix-archlinux-support go git \
+sudo pacman -Syu --noconfirm base-devel go git \
   xf86-video-amdgpu xorg --ignore xorg-server-xdmx firefox mpd dunst \
   nm-applet redshift feh blueman pasystray gpg-agent xbindkeys \
   xsettingsd barrier xhidecursor
-
-echo "[!] Review or modify pacman.conf (optional)..."
-read -p "    Press Enter to open /etc/pacman.conf with vim..."
-sudo vim /etc/pacman.conf
 
 echo "[+] Installing yay (AUR helper)..."
 cd /tmp/
