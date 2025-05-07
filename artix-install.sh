dc5b4f5129f3d3861074136402dac74c0b6015e1
0xhaxk <0xhaxk@noreply.codeberg.org>
2025-05-07 04:02:13 +0000

    fixed grub error

--- artix-install.sh
+++ artix-install.sh
@@ -91,7 +91,7 @@ echo "[+] Installing fonts..."
 pacman -S --noconfirm ttf-hack ttf-hack-nerd
 
 echo "[+] Installing GRUB bootloader..."
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "[✓] Setup complete inside chroot."
