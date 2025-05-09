3cf6b959f61a120bedb828164593957ab0778756
0xhaxk <0xhaxk@noreply.codeberg.org>
2025-05-09 23:41:55 +0000

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -66,12 +66,12 @@ PACMAN_PACKAGES=( \
     ttf-hack ttf-hack-nerd v4l2loopback-dkms v4l2loopback-utils \
     xdg-desktop-portal-wlr xdotool zathura-pdf-mupdf tmux \
     xcb-util-renderutil xcb-util-image uthash libconfig dunst pass audacity \
-    readline file img2pdf cups cups-pdf libinput-gestures \
+    readline file img2pdf cups cups-pdf \
 )
 
 # Packages for yay (AUR)
 YAY_PACKAGES=(
-    ugrep lsd gcr clipmenu-git ueberzugpp-nogl wkhtmltopdf-bin plata-theme
+    ugrep lsd gcr clipmenu-git ueberzugpp-nogl wkhtmltopdf-bin plata-theme libinput-gestures
 )
 
 # Install pacman packages
